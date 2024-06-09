92ee0eb964200754f60f4c50e0a8c524f8d4dfdd
Kowstubha9 <kowstubhachimirala@gmail.com>
2024-06-10 03:21:21 +0530

    Modified PS-6 MySQL solution file

--- problem-statement_code-files/PS-6/PS-6_using-MySQL.sql
+++ problem-statement_code-files/PS-6/PS-6_using-MySQL.sql
@@ -3,19 +3,19 @@
     USE sports_db;
 
 -- Creation of player information table
-    CREATE TABLE Player_information (SerialID INT PRIMARY KEY, Player VARCHAR(60), Team VARCHAR(60), Position VARCHAR(60), Season INT); 
+    CREATE TABLE Player_information (SerialID INT, Player VARCHAR(60), Team VARCHAR(60), Position VARCHAR(60), Season INT, PRIMARY KEY(SerialID, Player)); 
 
 -- Creation of player demographics table
-    CREATE TABLE PlayerDemographics (SerialID INT, Age INT, Height FLOAT, Weight FLOAT, FOREIGN KEY(SerialID) REFERENCES Player_information(SerialID));
+    CREATE TABLE PlayerDemographics (SerialID INT, Player VARCHAR(60), Age INT, Height FLOAT, Weight FLOAT, FOREIGN KEY(SerialID, Player) REFERENCES Player_information(SerialID, Player));
 
 -- Creation of player stats table
-    CREATE TABLE PlayerStats (SerialID INT, Goals INT, Assists INT, YellowCards INT, RedCards INT, PassCompletionRate FLOAT, DistanceCovered FLOAT, Sprints INT, ShotsOnTarget INT, TacklesWon INT, CleanSheets INT, FOREIGN KEY(SerialID) REFERENCES Player_information(SerialID));
+    CREATE TABLE PlayerStats (SerialID INT, Player VARCHAR(60), Goals INT, Assists INT, YellowCards INT, RedCards INT, PassCompletionRate FLOAT, DistanceCovered FLOAT, Sprints INT, ShotsOnTarget INT, TacklesWon INT, CleanSheets INT, FOREIGN KEY(SerialID, Player) REFERENCES Player_information(SerialID, Player));
 
 -- Creation of player training info table
-    CREATE TABLE PlayerTrainingInfo (SerialID INT, TrainingHours FLOAT, EffectiveTraining FLOAT, FOREIGN KEY(SerialID) REFERENCES Player_information(SerialID));
+    CREATE TABLE PlayerTrainingInfo (SerialID INT, Player VARCHAR(60), TrainingHours FLOAT, EffectiveTraining FLOAT, FOREIGN KEY(SerialID, Player) REFERENCES Player_information(SerialID, Player));
 
 -- Creation of player performance table
-    CREATE TABLE PlayerPerformance (SerialID INT, PressurePerformanceImpact FLOAT, MatchPressure INT, FOREIGN KEY(SerialID) REFERENCES Player_information(SerialID));
+    CREATE TABLE PlayerPerformance (SerialID INT, Player VARCHAR(60), PressurePerformanceImpact FLOAT, MatchPressure INT, FOREIGN KEY(SerialID, Player) REFERENCES Player_information(SerialID, Player));
 
 -- Creation of player health table
-    CREATE TABLE PlayerHealth (SerialID INT, InjuryHistory INT, PlayerFatigue FLOAT, FatigueInjuryCorrelation FLOAT, FOREIGN KEY(SerialID) REFERENCES Player_information(SerialID));
+    CREATE TABLE PlayerHealth (SerialID INT, Player VARCHAR(60), InjuryHistory INT, PlayerFatigue FLOAT, FatigueInjuryCorrelation FLOAT, FOREIGN KEY(SerialID, Player) REFERENCES Player_information(SerialID, Player));
